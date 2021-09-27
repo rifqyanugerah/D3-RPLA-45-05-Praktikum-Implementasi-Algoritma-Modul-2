ab3452f2a4510cddb814247304ff63f5ed59252f
Rifqy Anugerah <rifqyanugerah@student.telkomuniversity.ac.id>
2021-09-27 22:40:25 +0700

    TugasPraktikum.java

---
+++ Praktikum 2/TugasPraktikum.java
@@ -0,0 +1,9 @@
+public class TugasPraktikum {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama : Rifqy Anugerah");
+		System.out.println("Kelas: D3 RPLA 45-05");
+		System.out.println("NIM  : 6706213095");
+	}
+}
+	
